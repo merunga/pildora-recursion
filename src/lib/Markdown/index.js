6ac61d50d73c3dfcdb211e8d26a4f25db1f74edd
merunga <merunga@gmail.com>
2019-02-21 11:21:28 -0500

    notes

--- src/lib/Markdown/index.js
+++ src/lib/Markdown/index.js
@@ -1,1 +1,4 @@
-export default ({ children }) => <ReactMarkdown source={str} />;
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+export default ({ children }) => <ReactMarkdown source={children} />;
